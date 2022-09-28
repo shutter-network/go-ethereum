f6c7be6d367bffff5ee2a461ed61b6ad82913025
Max Langenfeld <max@brainbot.com>
2022-09-28 17:40:55 +0200

    Add byte-encoding for DecryptedPayload

--- core/types/decrypted_payload.go
+++ core/types/decrypted_payload.go
@@ -4,6 +4,7 @@ import (
 	"math/big"
 
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/rlp"
 )
 
 type DecryptedPayload struct {
@@ -30,4 +31,14 @@ func (p *DecryptedPayload) AsMessage(tx *Transaction, signer Signer) (Message, e
 		nil,           // access list
 		false,         // is fake
 	), nil
+}
+
+func (p *DecryptedPayload) Encode() ([]byte, error) {
+	return rlp.EncodeToBytes(*p)
+}
+
+func DecodeDecryptedPayload(b []byte) (*DecryptedPayload, error) {
+	p := &DecryptedPayload{}
+	err := rlp.DecodeBytes(b, p)
+	return p, err
 }
